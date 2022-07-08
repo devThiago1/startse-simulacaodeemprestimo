09fbc9d843a284ab28eddcf80fef122c9512f1bc
Thiago Calmon <thiago.calmon4@gmail.com>
2022-07-08 00:07:39 -0300

    75%

---
+++ financiamento.js
@@ -0,0 +1,31 @@
+import {Parcela} from './parcela';
+
+class Financiamento {
+    #taxaJuros; //juros mensais
+    #prazo; //em meses
+    #parcelas = [];
+    constructor(valor,entrada,taxajuros,prazo){
+        this.taxajuros = taxajuros;
+        this.#prazo = prazo;
+        //composição - financiamento possui ou tem parcelas
+        this.#parcelas.push(new Parcela(0,0,0,0,valor - entrada));
+    }
+
+    static calcJuros(valor, taxajuros){
+        return valor * (taxajuros /100);
+    }
+
+    calcParcelasMensais( ){
+        let saldo = this.#parcelas[this.#parcelas.length-1].getSaldo();
+        let prazo = this.#prazo - (this.#parcelas.length - 1);
+        let amortizacao = saldo / prazo;
+        for(let i = 0; i < prazo; i++) {
+            const numero = this.#parcelas.length;
+            const juros = Financiamento.calcJuros(saldo, this.#taxaJuros);
+            const valor = juros + amortizacao;
+            saldo -= amortizacao;
+            if(saldo < 0) {saldo = 0};
+            this.#parcelas.push(new Parcela(numero, valor, juros, amortizacao, saldo))
+        }
+    }
+}
---
+++ parcela.js
@@ -0,0 +1,19 @@
+export class Parcela {
+    #numero;
+    #valor;
+    #juros;
+    #amortizacao;
+    #saldo;
+    constructor(numero,valor, juros,amortizacao,saldo){
+        this.#numero = numero;
+        this.#valor = valor;
+        this.#juros = juros;
+        this.#amortizacao = amortizacao;
+        this.#saldo = saldo
+
+    }
+
+    getSaldo(){
+        return this.#saldo;
+    }
+}
